19179386a1a127c7a954f7bb94fbf8cdf7987e17
MircoSut <mircosut76@gmail.com>
2023-12-11 11:05:38 +0100

    created memory card and game classes

--- app/src/main/java/com/mcsut/mymemory/MainActivity.kt
+++ app/src/main/java/com/mcsut/mymemory/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.mcsut.mymemory
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mcsut.mymemory.models.BoardSize
+import com.mcsut.mymemory.models.MemoryCard
+import com.mcsut.mymemory.models.MemoryGame
 import com.mcsut.mymemory.utils.DEFAULT_ICONS
 
 //import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
@@ -26,10 +33,14 @@ class MainActivity : ComponentActivity() {
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-        val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
-        val randomizedImages = (chosenImages + chosenImages).shuffled()
+        val memoryGame = MemoryGame(boardSize)
 
-        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, randomizedImages)
+        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener {
+            override fun onCardClicked(position: Int) {
+                Log.i(TAG, "Card clicked $position")
+            }
+
+        })
         rvBoard.setHasFixedSize(true)
         rvBoard.layoutManager = GridLayoutManager(this,boardSize.getWidth())
     }
--- app/src/main/java/com/mcsut/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/mcsut/mymemory/MemoryBoardAdapter.kt
@@ -10,12 +10,14 @@ import android.widget.ImageButton
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.mcsut.mymemory.models.BoardSize
+import com.mcsut.mymemory.models.MemoryCard
 import kotlin.math.min
 
 class MemoryBoardAdapter(
     private val context: Context,
     private val boardSize: BoardSize,
-    private val cardImages: List<Int>
+    private val cards: List<MemoryCard>,
+    private val cardClickListener: CardClickListener
 ) :
 
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
@@ -23,6 +25,10 @@ class MemoryBoardAdapter(
     companion object {
         private const val MARGIN_SIZE = 10
         private const val TAG = "MemoryBoardAdapter"
+    }
+
+    interface CardClickListener {
+        fun onCardClicked(position: Int)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -48,9 +54,11 @@ class MemoryBoardAdapter(
         private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
 
         fun bind(position: Int) {
-            imageButton.setImageResource(cardImages[position])
+            val memoryCard = cards[position]
+            imageButton.setImageResource(if (memoryCard.isFaceUp) memoryCard.identifier else R.drawable.ic_launcher_background)
             imageButton.setOnClickListener {
                 Log.i(TAG, "Click on position $position")
+                cardClickListener.onCardClicked(position)
             }
         }
     }
